d7702832575fe21024dbf4e4a309305c615920a3
Jason Butters <jnbutters@gmail.com>
2021-10-02 08:27:55 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,8 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/jnbutters/s3-bucket/aws"
+  version = "2.2.0"
+  # insert required variables here
+  bucket_prefix = var.prefix
+  acl    = "private"
+
+}	
